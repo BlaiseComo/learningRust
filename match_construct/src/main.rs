cae2c6dacef673094c60179fa39b0b5928bc77ec
BlaiseComo <comoblaise@gmail.com>
2022-10-25 13:39:43 -0600

    Small Changes

--- match_construct/src/main.rs
+++ match_construct/src/main.rs
@@ -47,4 +47,18 @@ fn main() {
 
     println!("The variable none is {:?}", none);
 
+    // if let syntax
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+
 }
